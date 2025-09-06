3a2cbe4c676506564115fdb9763f239cb6156fb6
Lucas Gruwez <gruwez.lucas@gmail.com>
2025-09-06 17:36:34 +1000

    Fix importing

--- src/grutils/plotting/__init__.py
+++ src/grutils/plotting/__init__.py
@@ -1,4 +1,4 @@
-from . import arrow
-from . import lines
-from . import error_band
-from . import complex_plot
+from .arrow import plot_directed
+from .lines import plot_variable_color, plot_variable_width
+from .error_band import plot_error_band
+from .complex_plot import plot_complex_map, plot_complex_map_forward
